dd04595e8d7a3300ebba81893f105499259abb9d
itsfriendmeat <itsmeat@duck.com>
2024-08-01 16:37:13 -0400

    Added Decap admin as passthrough directory

--- eleventy.config.js
+++ eleventy.config.js
@@ -15,6 +15,7 @@ module.exports = function(eleventyConfig) {
 	// Copy the contents of the `public` folder to the output folder
 	// For example, `./public/css/` ends up in `_site/css/`
 	eleventyConfig.addPassthroughCopy({
+		"./admin":"/admin",
 		"./public/": "/",
 		"./node_modules/prismjs/themes/prism-okaidia.css": "/css/prism-okaidia.css"
 	});
